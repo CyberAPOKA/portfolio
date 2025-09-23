829fd53744f8313a6c1b40cc4793a7a708f02076
CyberAPOKA <oficialsteffens@hotmail.com>
2025-09-23 01:35:10 -0300

    themes preview.

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -14,11 +14,24 @@
                             Theme
                         </div>
                         <ul tabindex="0"
-                            class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-h-96 overflow-y-auto">
+                            class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-screen max-w-md lg:max-w-3xl max-h-96 overflow-y-auto">
                             <li v-for="theme in themes" :key="theme">
-                                <a @click="changeTheme(theme)" :class="{ 'active': currentTheme === theme }">
-                                    {{ theme }}
-                                </a>
+                                <button @click="changeTheme(theme)" class="gap-3 px-2 w-full text-left"
+                                    :class="{ 'active': currentTheme === theme }">
+                                    <div :data-theme="theme"
+                                        class="bg-base-100 grid shrink-0 grid-cols-2 gap-0.5 rounded-md p-1 shadow-sm">
+                                        <div class="bg-base-content size-1 rounded-full"></div>
+                                        <div class="bg-primary size-1 rounded-full"></div>
+                                        <div class="bg-secondary size-1 rounded-full"></div>
+                                        <div class="bg-accent size-1 rounded-full"></div>
+                                    </div>
+                                    <div class="w-32 truncate">{{ theme }}</div>
+                                    <svg v-if="currentTheme === theme" xmlns="http://www.w3.org/2000/svg" width="16"
+                                        height="16" viewBox="0 0 24 24" fill="currentColor" class="h-3 w-3 shrink-0">
+                                        <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z">
+                                        </path>
+                                    </svg>
+                                </button>
                             </li>
                         </ul>
                     </div>
